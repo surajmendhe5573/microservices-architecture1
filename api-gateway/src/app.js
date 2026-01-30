9f0fe3b5e2e3f53d77d3c5ece74cfce638750a9c
surajmendhe5573 <surajmendhe470@gmail.com>
2026-01-30 12:40:39 +0530

    api-gateway updates

--- api-gateway/src/app.js
+++ api-gateway/src/app.js
@@ -1,34 +1,3 @@
-// import express from "express";
-// import cors from "cors";
-// import helmet from "helmet";
-// import morgan from "morgan";
-// import compression from "compression";
-
-// import { registerProxies } from "./proxy/registerProxies.js";
-// import notFound from "./middlewares/default/notFound.js";
-// import errorHandler from "./middlewares/default/errorHandler.js";
-// import { responseFormatter } from "./middlewares/default/responseFormater.js";
-
-// const app = express();
-
-// app.use(helmet());
-// app.use(morgan("dev"));
-// app.use(cors());
-// // app.use(express.json());
-// app.use(compression());
-
-// app.use(responseFormatter);
-
-// app.get("/", (req, res) => {
-//   res.send("API Gateway is running 🚀");
-// });
-
-// registerProxies(app); // 🔥 THIS LINE REPLACES ALL ROUTES
-
-// app.use(notFound);
-// app.use(errorHandler);
-
-// export default app;
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
@@ -45,17 +14,16 @@ const app = express();
 app.use(helmet());
 app.use(morgan("dev"));
 app.use(cors());
+// app.use(express.json());
 app.use(compression());
-app.use(responseFormatter);
 
-// ✅ Do NOT parse JSON for proxy routes
-// app.use("/api/v1", express.json());
+app.use(responseFormatter);
 
 app.get("/", (req, res) => {
   res.send("API Gateway is running 🚀");
 });
 
-registerProxies(app);
+registerProxies(app); 
 
 app.use(notFound);
 app.use(errorHandler);
